e52f44dae9f1360b7e1ab110d5afd6011d797f3b
Kenneth Dait <kpdait@gmail.com>
2018-10-13 07:17:42 -0500

    Initial Commit

---
+++ Noted/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Noted
+//
+//  Created by kend on 10/13/18.
+//  Copyright © 2018 kenanigans.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
