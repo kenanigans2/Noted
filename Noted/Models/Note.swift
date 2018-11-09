7f117ae5c88d9a30e4e892e7159a46bdf16d0fe6
Kenneth Dait <kpdaitdev@icloud.com>
2018-11-09 01:25:31 -0600

    added note implementation and tests

--- Noted/Models/Note.swift
+++ Noted/Models/Note.swift
@@ -7,3 +7,69 @@
 //
 
 import Foundation
+
+enum DateMetadataType: Int
+{ case Created, LastModified, LastAccessed }
+
+struct Note {
+    
+    // MARK:- PRIVATE PROPERTIES
+    
+    private var contentData: (title: String?, content: String)
+    {
+        didSet
+        {
+            let timestamp = Date()
+            dateMetadata.lastModified = timestamp
+        }
+    }
+    
+    private var dateMetadata: (created: Date, lastModified: Date, lastAccessed: Date)
+    
+    // MARK:- INIT
+    
+    init(title: String?, content: String)
+    {
+        self.contentData = (title: title, content: content)
+        let timestamp = Date()
+        dateMetadata.created = timestamp
+        dateMetadata.lastModified = timestamp
+        dateMetadata.lastAccessed = timestamp
+    }
+    
+    // MARK:- PUBLIC METHODS
+    
+    func getDateMetadata(_ type: DateMetadataType) -> Date
+    {
+        switch type
+        {
+        case .Created: return self.dateMetadata.created
+        case .LastModified: return self.dateMetadata.lastModified
+        case .LastAccessed: return self.dateMetadata.lastAccessed
+        }
+    }
+    
+    mutating func readNote() -> (String?, String)
+    {
+        dateMetadata.lastAccessed = Date()
+        return (contentData.title, contentData.content)
+    }
+    
+}
+
+extension Note
+{
+    // MARK:- PUBLIC COMPUTED PROPERTIES
+    
+    var title: String?
+    {
+        mutating get { dateMetadata.lastAccessed = Date(); return contentData.title }
+        set { contentData.title = newValue }
+    }
+    
+    var content: String
+    {
+        mutating get { dateMetadata.lastAccessed = Date(); return contentData.content }
+        set { contentData.content = newValue }
+    }
+}
--- NotedTests/Test Models/NoteStructTests.swift
+++ NotedTests/Test Models/NoteStructTests.swift
@@ -7,27 +7,125 @@
 //
 
 import XCTest
+@testable import Noted
 
-class NoteStructTests: XCTestCase {
+class NoteStructTests: XCTestCase
+{
 
-    override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    override func setUp()
+    { super.setUp() }
+
+    override func tearDown()
+    { super.tearDown() }
+
+    // MARK:- NOTE WITH TITLE AND CONTENT
+    
+    func testInit_NoteWithTitleAndContent()
+    {
+        let testTitle = "test title"
+        let testContent = "test content"
+        var testNote = Note(title: testTitle, content: testContent)
+        XCTAssertNotNil(testNote)
+        XCTAssertEqual(testNote.title, testTitle)
+        XCTAssertEqual(testNote.content, testContent)
     }
-
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    
+    func testInit_NoteTitleIsOptional()
+    {
+        let testTitle = "test title"
+        let testContent = "test content"
+        var noteWithoutTitle = Note(title: nil, content: testContent)
+        var noteWithTitle = Note(title: testTitle, content: testContent)
+        XCTAssertNil(noteWithoutTitle.title)
+        XCTAssertNotNil(noteWithTitle.title)
+        XCTAssertEqual(noteWithTitle.title, testTitle)
     }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    
+    // MARK:- UPDATES TITLE AND CONTENT
+    
+    func testUpdate_UpdatesTitle()
+    {
+        let testTitle1 = "test title 1"
+        let testTitle2 = "test title 2"
+        var testNote = Note(title: testTitle1, content: "test")
+        XCTAssertEqual(testNote.title, testTitle1)
+        testNote.title = testTitle2
+        XCTAssertEqual(testNote.title, testTitle2)
     }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    func testUpdate_UpdatesContent()
+    {
+        let testContent1 = "test content 1"
+        let testContent2 = "test content 2"
+        var testNote = Note(title: nil, content: testContent1)
+        XCTAssertEqual(testNote.content, testContent1)
+        
+        testNote.content = testContent2
+        XCTAssertEqual(testNote.content, testContent2)
+    }
+    
+    // MARK:- DATE METADATA
+    
+    func testDate_NoteInitializesWithSameDates()
+    {
+        let testNote = Note(title: nil, content: "test")
+        let dateCreatedQueried = testNote.getDateMetadata(.Created)
+        XCTAssertNotNil(dateCreatedQueried)
+        let dateLastModifiedQueried = testNote.getDateMetadata(.LastModified)
+        XCTAssertNotNil(dateLastModifiedQueried)
+        XCTAssertEqual(dateLastModifiedQueried, dateCreatedQueried)
+        let dateLastAccessedQueried = testNote.getDateMetadata(.LastAccessed)
+        XCTAssertNotNil(dateLastAccessedQueried)
+        XCTAssertEqual(dateLastAccessedQueried, dateCreatedQueried)
+    }
+    
+    func testDate_UpdatingNoteProperties_UpdatesDateLastModified()
+    {
+        let title1 = "title 1"
+        let title2 = "title 2"
+        var testNote = Note(title: title1, content: "test content")
+        let dateCreatedQueried = testNote.getDateMetadata(.Created)
+        let dateLastModifiedQueried1 = testNote.getDateMetadata(.LastModified)
+        XCTAssertEqual(dateCreatedQueried, dateLastModifiedQueried1)
+        testNote.title = title2
+        let dateLastModifiedQueried2 = testNote.getDateMetadata(.LastModified)
+        XCTAssertNotEqual(dateLastModifiedQueried2, dateLastModifiedQueried1)
+        XCTAssertTrue(dateLastModifiedQueried1 < dateLastModifiedQueried2)
+    }
+    
+    func testDate_UpdatingNoteProperties_DoesNotUpdateDateLastAccessed()
+    {
+        let title1 = "title 1"
+        let title2 = "title 2"
+        var testNote = Note(title: title1, content: "test content")
+        let dateCreatedQueried = testNote.getDateMetadata(.Created)
+        let dateLastAccessedQueried1 = testNote.getDateMetadata(.LastAccessed)
+        XCTAssertEqual(dateCreatedQueried, dateLastAccessedQueried1)
+        testNote.title = title2
+        let dateLastAccessedQueried2 = testNote.getDateMetadata(.LastAccessed)
+        XCTAssertEqual(dateLastAccessedQueried1, dateLastAccessedQueried2)
+    }
+    
+    func testDate_ReadingNoteProperties_UpdatesDateLastAccessed()
+    {
+        var testNote = Note(title: nil, content: "test content")
+        let dateLastAccessed1 = testNote.getDateMetadata(.LastAccessed)
+        
+        _ = testNote.readNote()
+        let dateLastAccessed2 = testNote.getDateMetadata(.LastAccessed)
+        
+        XCTAssertNotEqual(dateLastAccessed1, dateLastAccessed2)
+        XCTAssertTrue(dateLastAccessed1 < dateLastAccessed2)
+    }
+    
+    func testDate_ReadingNoteProperties_DoesNotUpdatesDateLastModified()
+    {
+        var testNote = Note(title: nil, content: "test content")
+        let dateLastModified1 = testNote.getDateMetadata(.LastModified)
+        
+        _ = testNote.readNote()
+        let dateLastModified2 = testNote.getDateMetadata(.LastModified)
+        XCTAssertEqual(dateLastModified1, dateLastModified2)
     }
 
 }
